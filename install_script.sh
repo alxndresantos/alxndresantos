969281acc37e64aba09e3db414b19f218541e2b9
alxndresantos <49498302+alxndresantos@users.noreply.github.com>
2025-09-21 22:21:08 -0300

    Update install_script.sh
    teste2

--- install_script.sh
+++ install_script.sh
@@ -11,17 +11,19 @@ if [ ! -f "$JSON_FILE" ]; then
   exit 1
 fi
 
-# Verifica se loadbalances contém "floating_ip"
-if grep -q '"floating_ip"' "$JSON_FILE"; then
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - Loadbalances preenchido. Executando fluig_sust.sh..." | tee -a "$LOG_FILE"
-  cd /tmp || exit 1
-  curl -s -o fluig_sust.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fluig_sust.sh
-  sh /tmp/fluig_sust.sh
-  rm -f /tmp/fluig_sust.sh
-else
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - Loadbalances vazio. Executando fluig_sust_std.sh..." | tee -a "$LOG_FILE"
+# Pega a quantidade de itens em loadbalances
+COUNT=$(jq '.loadbalances | length' "$JSON_FILE" 2>/dev/null)
+
+if [ "$COUNT" -eq 0 ]; then
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Loadbalances vazio ([]). Executando fluig_sust_std.sh" | tee -a "$LOG_FILE"
   cd /tmp || exit 1
   curl -s -o fluig_sust_std.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fluig_sust_std.sh
   sh /tmp/fluig_sust_std.sh
   rm -f /tmp/fluig_sust_std.sh
+else
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Loadbalances contém $COUNT objeto(s). Executando fluig_sust.sh" | tee -a "$LOG_FILE"
+  cd /tmp || exit 1
+  curl -s -o fluig_sust.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fluig_sust.sh
+  sh /tmp/fluig_sust.sh
+  rm -f /tmp/fluig_sust.sh
 fi
