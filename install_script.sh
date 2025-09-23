0b481fc7285916b6c728b87106ffb3a15478d38d
alxndresantos <49498302+alxndresantos@users.noreply.github.com>
2025-09-22 22:53:54 -0300

    Update install_script.sh
    correção

--- install_script.sh
+++ install_script.sh
@@ -15,15 +15,15 @@ fi
 COUNT=$(jq '.loadbalances | length' "$JSON_FILE" 2>/dev/null)
 
 if [ "$COUNT" -eq 0 ]; then
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - Loadbalances vazio ([]). Executando fluig_sust_std.sh" | tee -a "$LOG_FILE"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Loadbalances vazio ([]). Executando fluig_sust_only_instance.sh" | tee -a "$LOG_FILE"
   cd /tmp || exit 1
-  curl -s -o fluig_sust_std.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fluig_sust_only_instance.sh
+  curl -s -o fluig_sust_only_instance.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fluig_sust_only_instance.sh
   sh /tmp/fluig_sust_only_instance.sh
   rm -f /tmp/fluig_sust_only_instance.sh
 else
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - Loadbalances contém $COUNT objeto(s). Executando fluig_sust.sh" | tee -a "$LOG_FILE"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Loadbalances contém $COUNT objeto(s). Executando fluig_sust_multi_instances.sh" | tee -a "$LOG_FILE"
   cd /tmp || exit 1
-  curl -s -o fluig_sust.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fluig_sust_multi_instances.sh
+  curl -s -o fluig_sust_multi_instances.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fluig_sust_multi_instances.sh
   sh /tmp/fluig_sust_multi_instances.sh
   rm -f /tmp/fluig_sust_multi_instances.sh
 fi
