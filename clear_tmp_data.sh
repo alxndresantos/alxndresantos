4a88c5ecc9914a137f55a64e1a95191a6938907e
alxndresantos <49498302+alxndresantos@users.noreply.github.com>
2025-09-30 22:16:29 -0300

    Create clear_tmp_data.sh
    clear_tmp_data.sh

---
+++ clear_tmp_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Load fluig variables
+. ./fluig.conf
+
+# Clear temporary folders. 
+if [ -d  $JBOSS_SERVER/data/ ]; then   # Check if folder exists
+    echo "Removing $JBOSS_SERVER/data/"
+    rm -rf $JBOSS_SERVER/data/
+    echo "Done"
+else
+    echo "data folder not found. Nothing happened."
+fi
+
+if [ -d  $JBOSS_SERVER/tmp/ ]; then   # Check if folder exists
+    echo "Removing $JBOSS_SERVER/tmp/"
+    rm -rf $JBOSS_SERVER/tmp/
+    echo "Done"
+else
+    echo "tmp Folder not found. Nothing happened."
+fi
