766974194c66785067fd3868156799f75293946f
alxndresantos <49498302+alxndresantos@users.noreply.github.com>
2025-09-30 22:40:35 -0300

    Update rotate_log.sh
    /volume/CloudFluig/fluig.conf

--- rotate_log.sh
+++ rotate_log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Load fluig variables
-. ./fluig.conf
+/volume/CloudFluig/fluig.conf
 
 # Rotate server.log - required to check when fluig is up and running
 mv $JBOSS_SERVER/log/server.log $JBOSS_SERVER/log/server.log.$(date +%Y-%m-%d-%H-%M-%S)
