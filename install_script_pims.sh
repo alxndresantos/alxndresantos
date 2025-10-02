cde299ed0ccb0af8191526d2082505ac9d8f8e1b
alxndresantos <49498302+alxndresantos@users.noreply.github.com>
2025-10-02 15:24:35 -0300

    Update install_script_pims.sh
    Update install_script_pims.sh

--- install_script_pims.sh
+++ install_script_pims.sh
@@ -1,1 +1,24 @@
-install_script_pims.sh
+
+#!/bin/bash
+# Desenvolvido por Alexandre dos Santos (Coxa) - 20-09-2025
+
+# Cria a pasta e baixa os scripts
+mkdir -p /volume/CloudAGRO && cd /volume/CloudAGRO/ || exit 1
+
+# Cria arquivos vazios e aplica permissão
+touch check_wildfly.sh fix_visudo.sh fonte.sh
+chmod +x check_wildfly.sh fix_visudo.sh fonte.sh
+
+# Baixa os scripts atualizados do GitHub
+curl -s -o check_wildfly.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/check_wildfly.sh
+curl -s -o fix_visudo.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fix_visudo.sh
+curl -s -o fonte.sh https://raw.githubusercontent.com/alxndresantos/alxndresantos/main/fonte.sh
+
+
+# Remove bloco antigo do crontab e adiciona novamente
+(crontab -l 2>/dev/null | sed '/^#Cloud_sustentação$/,/^#Cloud_sustentação$/d'; cat <<'EOF'
+#Cloud_sustentação
+* * * * * /bin/bash /volume/CloudAGRO/check_wildfly.sh >> /volume/CloudAGRO/check_wildfly.log 2>&1
+#Cloud_sustentação
+EOF
+) | crontab -
